8aa5b74b5af2e6ec666d0d3da63dccac230d2c26
Iskender <mederov.iskendr@gmail.com>
2023-07-19 16:55:46 +0600

    first commit

--- src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
+++ src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
@@ -1,13 +1,26 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FunctionValueFinder {
     public boolean isFunctionValuePresent(List<Integer> sourceList, int requiredValue) {
         Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
+        if (functionMap.containsValue(requiredValue)){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     private Map<Integer, Integer> calculateFunctionMap(List<Integer> sourceList) {
+        Map<Integer,Integer> calculatedMap = new HashMap<>();
+        for (Integer number: sourceList){
+            int key = number;
+            int value = number*5+2;
+            calculatedMap.put(key,value);
+        }
+        return calculatedMap;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,24 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> wordMap = new HashMap<>();
+
+        // Split the sentence into words using non-word characters as delimiters
+        String[] words = sentence.toLowerCase().split("\\s+");
+
+        for (String word : words) {
+            word = word.replaceAll("^\\p{Punct}+|\\p{Punct}+$", "");
+            // Increment the count if the word already exists in the map
+            // Otherwise, add the word to the map with an initial count of 1
+            wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
+        }
+
+        return wordMap;
     }
 }
